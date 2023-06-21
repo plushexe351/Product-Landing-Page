0753fe76a00ba2d174d3ca7541d6c11213bf2bfc
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-06-22 00:44:28 +0530

    Fixed hamburger menu and made it responsive

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const hamburger = document.getElementById('hamburger');
 const header = document.getElementById('header-content');
 const navbar = document.getElementById('navbar');
+hamburger.addEventListener('click', () => {
+    header.classList.toggle('active');
+})
