a398449a86d6112aa4d35cda127dedb81b74671c
Ushnish Tapaswi <126304736+plushexe351@users.noreply.github.com>
2023-06-29 00:04:42 +0530

    fixed bugs and added a video element

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById('hamburger');
 const header = document.getElementById('header-content');
-const navbar = document.getElementById('navbar');
 hamburger.addEventListener('click', () => {
     header.classList.toggle('active');
 })
